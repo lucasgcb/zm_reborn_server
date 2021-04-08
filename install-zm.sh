2b600c1dfb6504dc2fb6548d278d1948e2808fff
Lucas Gabriel Coliado Bandeira <lucascoliado@hotmail.com>
2021-04-08 08:56:43 -0300

    Add subtlety

--- install-zm.sh
+++ install-zm.sh
@@ -2,8 +2,8 @@
 curl "{$1}" -L  --output zm.tz.gz
 tar -zxvf zm.tz.gz
 
-echo "hostname \"King Kong vs Xotazilla\" " >> zombie_master_reborn/cfg/server.cfg
-echo "rcon_password \"pinto\" " >>  zombie_master_reborn/cfg/server.cfg
+echo "hostname \"King Kong vs Botazilla\" " >> zombie_master_reborn/cfg/server.cfg
+echo "rcon_password \"kurwa\" " >>  zombie_master_reborn/cfg/server.cfg
 echo "sv_downloadurl \"http://46.4.98.131/fastdl/zombie_master/maps/"  >> zombie_master_reborn/cfg/server.cfg
 echo "sv_allowdownload 1" >>  zombie_master_reborn/cfg/server.cfg
 echo "sv_allowupload 1"  >>  zombie_master_reborn/cfg/server.cfg
